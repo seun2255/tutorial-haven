3caa3691001b8efbf474cb786006b69cb8970f3a
Primal <81525899+Seun2255@users.noreply.github.com>
2023-08-09 08:17:53 +0100

    feat: created a module for storing icons needed

---
+++ app/_assets/icons/icons.js
@@ -0,0 +1,43 @@
+import cancel from "./cancel.svg";
+import check from "./check.svg";
+import logo from "./logo.svg";
+import settings from "./settings.svg";
+import search from "./search.svg";
+import upload from "./upload.svg";
+import share from "./share.svg";
+import trash from "./trash.svg";
+import expand from "./expand.svg";
+import drag from "./drag.svg";
+import menu from "./menu.svg";
+import profile from "./profile.svg";
+import schedule from "./schedule.svg";
+import back from "./back.svg";
+import thumbnail from "./thumbnail.svg";
+import tick from "./tick.svg";
+import lock from "./lock.svg";
+import scrollArrow from "./scrollArrow.svg";
+
+const icons = {
+  cancel,
+  check,
+  logo,
+  settings,
+  search,
+  upload,
+  share,
+  trash,
+  schedule,
+  expand,
+  drag,
+  box,
+  menu,
+  profile,
+  back,
+  thumbnail,
+  tick,
+  licence,
+  lock,
+  scrollArrow,
+};
+
+export default icons;
---
+++ app/_assets/images/images.js
@@ -0,0 +1,39 @@
+import lovedBy from "./Loved By.svg";
+import company from "./COMPANY.svg";
+import frame2 from "./Frame 9405.svg";
+import compareX from "./CompareX.svg";
+import demoVideo from "./DEMO VIDEO.svg";
+import crown from "./Fill 7.svg";
+import frame1 from "./Frame 2294.svg";
+import upwork from "./Upwork.svg";
+import vector from "./Vector.svg";
+import followers from "./200K followers.svg";
+import atinuke from "./Atinuke.svg";
+import damilare from "./Damilare.svg";
+import drag from "./drag.svg";
+import michael from "./michael.svg";
+import woman from "./woman.svg";
+import dashboard from "./dashboard.svg";
+import or from "./OR-line.svg";
+
+const images = {
+  lovedBy,
+  company,
+  frame1,
+  frame2,
+  compareX,
+  demoVideo,
+  vector,
+  upwork,
+  crown,
+  followers,
+  atinuke,
+  damilare,
+  drag,
+  woman,
+  michael,
+  dashboard,
+  or,
+};
+
+export default images;
